11c8e5d4e0e267e80050fdd8a24d580544278fc8
alex <alex@TeamCity>
2019-07-20 13:29:50 +0000

    TeamCity change in 'Mamkin Devops' project: VCS roots of 'Manual Test' build configuration were updated

--- .teamcity/patches/buildTypes/ManualTest.kts
+++ .teamcity/patches/buildTypes/ManualTest.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("ManualTest")
     name = "Manual Test"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
